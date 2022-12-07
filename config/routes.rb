20a0c99fa43d879e5ec3bb5f57b3f2e65d0f5b7c
urotansh <114655455+urotansh@users.noreply.github.com>
2022-12-07 12:42:17 +0000

    Add root to: 'top#index' > routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'top#index' #= root :to => 'top#index'
   get 'top/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
